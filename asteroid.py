7ae53129cf0d7146327c2c0dda40dabd83dcfe60
InfinityCoda <charles.cullymore@gmail.com>
2025-07-06 18:02:07 -0400

    Added game logic, player and asteroid handling and behavior. Need to work out a bug where new asteroids spawn at the maximum allowed angle instead of being randomized.

---
+++ asteroid.py
@@ -0,0 +1,48 @@
+import pygame
+import random
+from circleshape import CircleShape
+from constants import *
+
+class Asteroid(CircleShape):
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, radius)
+
+
+    def draw(self, screen):
+        pygame.draw.circle(screen, "white", self.position, self.radius, width=2)
+
+    
+    def update(self, dt):
+        self.position += (self.velocity * dt)
+
+
+    def split(self):
+        self.kill()
+        if self.radius < ASTEROID_MIN_RADIUS:
+            return
+        random_angle = random.uniform(20, 50)
+        new_vector = self.velocity.rotate(random_angle) * ASTEROID_SPEED_MULT
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+        return (new_radius, self.position, new_vector), (new_radius, self.position, -new_vector)
+        # 
+
+
+    ### Below code is taken from the solution file
+    ### Study this and try to understand how they accomplished this and why it's written this way
+    # def split(self):
+    #     self.kill()
+
+    #     if self.radius <= ASTEROID_MIN_RADIUS:
+    #         return
+
+    #     # randomize the angle of the split
+    #     random_angle = random.uniform(20, 50)
+
+    #     a = self.velocity.rotate(random_angle)
+    #     b = self.velocity.rotate(-random_angle)
+
+    #     new_radius = self.radius - ASTEROID_MIN_RADIUS
+    #     asteroid = Asteroid(self.position.x, self.position.y, new_radius)
+    #     asteroid.velocity = a * 1.2
+    #     asteroid = Asteroid(self.position.x, self.position.y, new_radius)
+    #     asteroid.velocity = b * 1.2
--- main.py
+++ main.py
@@ -1,18 +1,71 @@
 import pygame
+import sys
 from constants import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
+    Shot.containers = (shots, updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+    asteroidfield = AsteroidField()
+
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH /2, SCREEN_HEIGHT /2)
+
+    dt = 0  # delta time
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        updatable.update(dt)
+
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                print("Game over!")
+                sys.exit()
+
+            for shot in shots:
+                if asteroid.collides_with(shot):
+                    # my asteroid.split() returns a tuple of 2 values, each a tuple of 3 values with the asteroid properties (radius, position, velocity)
+                    try:
+                        new_asteroid_1, new_asteroid_2 = asteroid.split() 
+                        # if the old asteroid was below min size, split returns None. this will cause a TypeError
+                        # upon unpacking, so we need to handle this
+                    except TypeError:
+                        break
+
+                    asteroidfield.spawn(*new_asteroid_1) 
+                    asteroidfield.spawn(*new_asteroid_2)
+
+                    ### From solution code, they use this one line instead of 3 as the split method handles spawning the new asteroids
+                    ### it doesn't return the new asteroid values, so there's no unpacking, and no need to handle the TypeError
+                    # asteroid.split()  
+                    shot.kill()
+
+                    
         screen.fill("black")
+        
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000  # limit game's framerate to 60 FPS
 
 
 if __name__ == "__main__":
